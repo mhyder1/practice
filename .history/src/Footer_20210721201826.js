362b3443b5766acb0dbe4ccd3c54d7b674b0960b
unknown <hyder1@gmail.com>
2021-07-21 20:59:34 -0400

    updated

--- .history/src/Footer_20210721201826.js
+++
@@ -1,7 +0,0 @@
-function Footer() {
-    return (
-      <h1 style={{marginTop: '90%'}}>Footer</h1>
-    );
-  }
-  
-  export default Footer;
--- .history/src/Footer_20210721201850.js
+++
@@ -1,7 +0,0 @@
-function Footer() {
-    return (
-      <h1 style={{marginTop: '80vh'}}>Footer</h1>
-    );
-  }
-  
-  export default Footer;
